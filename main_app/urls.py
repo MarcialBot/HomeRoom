ef2341113da42751ccbf3cac39ba320061ae3341
arthur-ryoo <jene0523@gmail.com>
2020-04-15 22:26:10 -0700

    Add create and index path for classroom

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
+    path('classrooms/create/', views.ClassroomCreate.as_view(),
+         name='classrooms_create'),
+    path('classrooms/', views.ClassroomList.as_view(), name='classrooms_index'),
     path('accounts/signup/', views.signup, name='signup'),
 ]

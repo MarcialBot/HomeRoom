22ad49b63d86eb10474a4f6eeb5d169704a5a2f5
arthur-ryoo <jene0523@gmail.com>
2020-04-18 11:09:37 -0700

    Add assignment model to django admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Profile, Classroom
+from .models import Profile, Classroom, Assignment
 
 admin.site.register(Profile)
 admin.site.register(Classroom)
+admin.site.register(Assignment)

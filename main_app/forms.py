f9484709f28f1d042781e00f8f5e11a035f36c4a
arthur-ryoo <jene0523@gmail.com>
2020-04-21 16:39:30 -0700

    Add classroom model form

--- main_app/forms.py
+++ main_app/forms.py
@@ -2,10 +2,16 @@ from django import forms
 from django.forms import ModelForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .models import Profile
+from .models import Profile, Classroom
 
 
 class ProfileForm(ModelForm):
     class Meta:
         model = Profile
         fields = ('user_type', 'address', 'phone_number')
+
+class ClassForm(ModelForm):
+    class Meta:
+        model = Classroom
+        fields = ['subject' , 'user']
+
